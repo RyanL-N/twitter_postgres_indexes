3feeb524a6e197f7ba3538412ed1a5682bf0445a
RyanL-N <ra.lee.nguyen@gmail.com>
2025-04-25 10:40:14 -0700

    saving progress after copying data over

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,29 +1,21 @@
 #!/bin/sh
 
-files='/data/tweets/geoTwitter21-01-01.zip
-/data/tweets/geoTwitter21-01-02.zip
-/data/tweets/geoTwitter21-01-03.zip
-/data/tweets/geoTwitter21-01-04.zip
-/data/tweets/geoTwitter21-01-05.zip
-/data/tweets/geoTwitter21-01-06.zip
-/data/tweets/geoTwitter21-01-07.zip
-/data/tweets/geoTwitter21-01-08.zip
-/data/tweets/geoTwitter21-01-09.zip
-/data/tweets/geoTwitter21-01-10.zip'
-
+files=$(find data/*)
 
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-# FIXME: copy your solution to the previous problem here
+# FIXME: implement this with GNU parallel
+	echo "$files" |  time parallel ./load_denormalized.sh
 
-# NOTE:
-# I have removed the pg_normalized code from this repo.
-# The only difference between pg_normalized and pg_normalized_batch is how the data is loaded.
-# Since pg_normalized_batch is faster,
-# we will use that code to load the data.
+echo '================================================================================'
+echo 'load pg_normalized'
+echo '================================================================================'
+# FIXME: implement this with GNU parallel
+    echo "$files" |  time parallel python3 -u load_tweets.py --db="postgresql://postgres:pass@localhost:5438" --inputs $file
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: copy your solution to the previous problem here
+# FIXME: implement this with GNU parallel
+    echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:5439 --inputs $file
